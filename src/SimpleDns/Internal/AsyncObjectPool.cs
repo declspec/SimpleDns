00a5ab2e45b5d6bddcea220b6fd409b623f3d7a1
declspec <jasonlarke@live.com.au>
2016-08-26 13:25:28 +0800

    Cleaned up AsyncObjectPool to make it more performant

--- src/SimpleDns/Internal/AsyncObjectPool.cs
+++ src/SimpleDns/Internal/AsyncObjectPool.cs
@@ -5,33 +5,28 @@ using System.Collections.Generic;
 
 namespace SimpleDns.Internal {
     public class AsyncObjectPool<T> where T : class {
+        private const int OBJECT_BORROWED = 1;
+        private const int OBJECT_AVAILABLE = 0;
+
         private readonly PoolItem[] _pool;
         private readonly Func<int, T> _factory;
         private readonly LinkedList<TaskCompletionSource<T>> _waiting;
-        private T _first;
     
         public AsyncObjectPool(int capacity, Func<int, T> factory) {
-            _first = null;
-            _pool = new PoolItem[capacity - 1];
+            _pool = new PoolItem[capacity];
             _waiting = new LinkedList<TaskCompletionSource<T>>();
             _factory = factory;
         }
 
         public async Task<T> Acquire(CancellationToken token) {
-            T instance = _first;
-            if (instance != null && instance == Interlocked.CompareExchange(ref _first, null, instance))
-                return instance;
-
-            return await AcquireSlow(token);
-        }
-
-        private async Task<T> AcquireSlow(CancellationToken token) {
             var localPool = _pool;
-            var instance = default(T);
 
             for(int i = 0; i < localPool.Length; ++i) {
-                if ((instance = Interlocked.CompareExchange(ref localPool[i].Value, null, localPool[i].Value)) != null)
-                    return instance;
+                if (Interlocked.CompareExchange(ref localPool[i].Borrowed, OBJECT_BORROWED, OBJECT_AVAILABLE) == OBJECT_AVAILABLE) {
+                    return localPool[i].Value != null
+                        ? localPool[i].Value
+                        : (localPool[i].Value = _factory(i));
+                }
             }
 
             var tcs = new TaskCompletionSource<T>();
@@ -62,34 +57,82 @@ namespace SimpleDns.Internal {
             // Deliberately avoid taking a lock on _waiting pre-emptively. Worst case
             // scenario is a recently added "waiter" gets skipped until the next
             // resource is free'd which is extremely unlikely.
-            if (_waiting.Count > 0) {
-                TaskCompletionSource<T> tcs;
-
-                lock(_waiting) {
-                    tcs = _waiting.First.Value;
-                    _waiting.RemoveFirst();
+            if (_waiting.Count == 0) {
+                for(int i = 0; i < _pool.Length; ++i) {
+                    // Each object should only be owned by one user at a time,
+                    // which means this read shouldn't need to be interlocked
+                    if (_pool[i].Value == obj) {
+                        Interlocked.Exchange(ref _pool[i].Borrowed, OBJECT_AVAILABLE);
+                        break;
+                    }
                 }
-
-                tcs.TrySetResult(obj);
             }
             else {
-                if (Interlocked.CompareExchange(ref _first, obj, null) != null)
-                    FreeSlow(obj);
+                TaskCompletionSource<T> tcs = null;
+
+                lock(_waiting) {
+                    // Double-read 'Count' as we may have lost a contentious lock
+                    // with only a single item in the list since our last read.
+                    if (_waiting.Count > 0) {
+                        tcs = _waiting.First.Value;
+                        _waiting.RemoveFirst();
+                    }
+                }
+
+                if (tcs != null)
+                    tcs.SetResult(obj);
             }
         }
 
-        private void FreeSlow(T obj) {
-            var localPool = _pool;
+        public void Remove(T obj) {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
 
-            for(int i = 0; i < localPool.Length; ++i) {
-                if (Interlocked.CompareExchange(ref localPool[i].Value, obj, null) == null)
+            // Same semantics as 'Free' in that only one PoolItem can own this resource
+            // at a time so there should be no need for interlocked reads
+            int i = 0;
+            for (; i < _pool.Length; ++i) {
+                if (_pool[i].Value == obj)
                     break;
+            }
+
+             // Was the item even in the pool?
+            if (i >= _pool.Length)
+                return;
+
+            TaskCompletionSource<T> tcs = null;
+
+            // If there are any pending 'waiters' we can allocate one a new instance
+            // and resolve it right away. 
+
+            // PERF: Can remove this if you don't care too much about the order that 
+            //  waiters are resolved. Without this, the PoolItem will be claimed by the
+            //  next call to 'Acquire' and the waiter will have to wait for a resource from
+            //  'Free' instead. There is the potential for a deadlock if there are waiters,
+            //  no further calls to Acquire occur, and every single current object is removed rather than freed.
+            if (_waiting.Count > 0) {
+                lock(_waiting) {
+                    // Double-read 'Count' as we may have lost a contentious lock
+                    // with only a single item in the list since our last read.
+                    if (_waiting.Count > 0) {
+                        tcs = _waiting.First.Value;
+                        _waiting.RemoveFirst();
+                    }
+                }
+            }
+
+            if (tcs != null) 
+                tcs.SetResult(_pool[i].Value = _factory(i));
+            else {
+                // Noone needs an object right now, just free the item slot
+                _pool[i].Value = null;
+                Interlocked.Exchange(ref _pool[i].Borrowed, OBJECT_AVAILABLE);
             }
         }
 
         private struct PoolItem {
             public T Value;
+            public int Borrowed;
         }
     }
-
 }
